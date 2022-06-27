183b6d1fb964a287c6dd846cc5da8179c6ca17c8
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2022-06-27 19:01:37 -0400

    Add files via upload
    About page structure and functionality finished. Finishing touches are needed.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import mbsSmall from '../assets/mbsSmall.png'
 import nav from '../styles/nav/nav.css'
 
 const Nav = () => {
+  const [about, setAbout] = useState(true)
+
+
+
+
+const AboutPage = () => {
+  const aboutId = document.getElementById('aboutId')
+  
+  if(about) {
+    setAbout(false)
+    console.log(about)
+  } else {
+    setAbout(true)
+    console.log(about)
+  }
+
+  if(about) {
+    aboutId.classList.add('about-page-active')
+  } else {
+    aboutId.classList.remove('about-page-active')
+  }
+}
+
+
+
   return (
     <div className='nav'>
+      <div id="aboutId" className='about-page' onClick={() => {AboutPage()}}>
+        <div className='about-title'>MATCHBOX STICKERS</div>
+        <div className='about-info'>Matchbox Stickers curates arist series sticker drops packaged in a proprietary matchbox.</div>
+        <div className='about-how-container'>
+          <div className='about-how-title'>HOW'S IT WORK?</div>
+          <div className='about-how-info'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium corrupti veniam molestiae quidem, nemo inventore explicabo quo maxime sunt placeat laborum repudiandae vitae consectetur beatae magnam perferendis delectus facilis ratione labore atque? Aliquid, similique! Ducimus nobis, deleniti expedita saepe odio cum fugit doloribus commodi quis dolor eligendi voluptas quas laudantium!</div>
+        </div>
+      </div>
       <div className='nav-container'>
         <div className='nav-about'>
-          <div className='nav-about-title'>ABOUT THE PROJECT</div>
+          <div className='nav-about-title' onClick={() => {AboutPage()}}>ABOUT THE PROJECT</div>
           </div>
         <div className='nav-img'><img src={mbsSmall}/></div>
 
