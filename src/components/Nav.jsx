80bc70f5a87bce22f6007f6af30796a7775db818
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2022-08-22 09:44:10 -0700

    Add files via upload
    Reducing lines of JSX and SCSS in the Nav. Nav cleanup not complete, I need to rename many of the divs to semantic elements

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,15 +6,12 @@ const Nav = () => {
   const [about, setAbout] = useState(true)
 
   const AboutPage = () => {
-
     const aboutId = document.getElementById('aboutId')
   
     if(about) {
       setAbout(false)
-      console.log(about)
     } else {
       setAbout(true)
-      console.log(about)
     }
 
     if(about) {
@@ -28,6 +25,7 @@ const Nav = () => {
 
   return (
     <div className='nav'>
+      {/* About Page Start */}
       <div id="aboutId" className='about-page' onClick={() => {AboutPage()}}>
         <div className='about-title'>MATCHBOX STICKERS</div>
         <div className='about-info'>Matchbox Stickers curates arist series sticker drops packaged in a proprietary matchbox.</div>
@@ -36,6 +34,8 @@ const Nav = () => {
           <div className='about-how-info'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium corrupti veniam molestiae quidem, nemo inventore explicabo quo maxime sunt placeat laborum repudiandae vitae consectetur beatae magnam perferendis delectus facilis ratione labore atque? Aliquid, similique! Ducimus nobis, deleniti expedita saepe odio cum fugit doloribus commodi quis dolor eligendi voluptas quas laudantium!</div>
         </div>
       </div>
+      {/* About Page End */}
+      {/* Nav Start */}
       <div className='nav-container'>
         <div className='nav-about'>
           <div className='nav-about-title' onClick={() => {AboutPage()}}>ABOUT THE PROJECT</div>
@@ -45,6 +45,7 @@ const Nav = () => {
           <div className='nav-account'>SHOP ALL</div>
         </div>
       </div>
+      {/* Nav End */}
     </div>
   )
 }
