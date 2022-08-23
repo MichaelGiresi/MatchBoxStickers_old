9b2bc5ac1fbd5dceab38a210d93e6183e2c9c958
MichaelGiresi <84191824+MichaelGiresi@users.noreply.github.com>
2022-08-23 10:48:19 -0700

    Add files via upload
    Nav now has semantic tags.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,8 +3,10 @@ import mbsSmall from '../assets/mbsSmall.png'
 import nav from '../styles/nav/nav.css'
 
 const Nav = () => {
+
   const [about, setAbout] = useState(true)
 
+  // About Page Toggle Function
   const AboutPage = () => {
     const aboutId = document.getElementById('aboutId')
   
@@ -34,18 +36,19 @@ const Nav = () => {
           <div className='about-how-info'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium corrupti veniam molestiae quidem, nemo inventore explicabo quo maxime sunt placeat laborum repudiandae vitae consectetur beatae magnam perferendis delectus facilis ratione labore atque? Aliquid, similique! Ducimus nobis, deleniti expedita saepe odio cum fugit doloribus commodi quis dolor eligendi voluptas quas laudantium!</div>
         </div>
       </div>
-      {/* About Page End */}
       {/* Nav Start */}
-      <div className='nav-container'>
-        <div className='nav-about'>
-          <div className='nav-about-title' onClick={() => {AboutPage()}}>ABOUT THE PROJECT</div>
-        </div>
-        <div className='nav-img'><img src={mbsSmall}/></div>
-        <div className='nav-account-cart-container'>
-          <div className='nav-account'>SHOP ALL</div>
-        </div>
-      </div>
-      {/* Nav End */}
+      <nav className='nav-container'>
+        <ul className='nav-container'>
+
+        <li className='nav-about'>
+          <a className='nav-about-title' onClick={() => {AboutPage()}}>ABOUT THE PROJECT</a>
+        </li>
+        <li className='nav-img'><img src={mbsSmall}/></li>
+        <li className='nav-account-cart-container'>
+          <a className='nav-account'>SHOP ALL</a>
+        </li>
+        </ul>
+      </nav>
     </div>
   )
 }
