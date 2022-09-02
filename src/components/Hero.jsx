af7a16f4bcb3d89f55ba0d459cdfecd7f53193c8
MichaelGiresi <mikegiresidev@gmail.com>
2022-09-02 19:35:08 -0400

    hero cleaned

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect, useRef } from 'react'
 import hero from '../styles/hero/hero.css'
 import VideoPlayer from '../scripts/videoPlayer'
 import video from '../assets/video.mp4'
-// import HeroVideoButtons from '../scripts/HeroVideoButtons'
 const Hero = () => {
 
   const videoElement = useRef(null)
@@ -21,7 +20,6 @@ const Hero = () => {
     heroButtonSixTime,
 
   } = VideoPlayer(videoElement)
-
 
 useEffect(() => {
 
@@ -71,32 +69,20 @@ useEffect(() => {
   
   return (
     <div className='hero'>
-      {/* <div className='hero-top'>1</div> */}
       <div className="hero-container">
-
-        
-
         <div className='hero-video-buttons-container'>
-
           <div className='hero-buttons-video-container'>
-
             <div className='hero-button-container'>
-
               <div className='hero-button-container-2'>
-
                 <button id='btn1' className='hero-button-1 ' onClick={heroButtonOneTime}></button>
                 <button id='btn2' className='hero-button-2' onClick={heroButtonTwoTime} ></button>
                 <button id="btn3" className='hero-button-3' onClick={heroButtonThreeTime}></button>
                 <button id="btn4" className='hero-button-4' onClick={heroButtonFourTime}></button>
                 <button id="btn5" className='hero-button-5' onClick={heroButtonFiveTime}></button>
                 <button id="btn6" className='hero-button-6' onClick={heroButtonSixTime}></button>
-
               </div>
-
             </div>
-            
             <div className='hero-video-container'>
-
               <video 
                 className="video"
                 src={video} 
@@ -107,71 +93,33 @@ useEffect(() => {
                 autoPlay
                 loop
               />
-
               <div className='video-controls'>
-
-                {/* <input 
-                className="video-slider"
-                type='range' 
-                min="0"
-                max="100"
-                value={playerState.progress}
-                onChange={(e) => handleVideoProgress(e)}
-                /> */}
-
-                {/* <select
-                className="video-speed-select"
-                value={playerState.speed}
-                onChange= {(e) => handleVideoSpeed(e)}
-                >
-
-                <option value="0.50">0.50</option>
-                <option value="1">1</option>
-                <option value="1.25">1.25</option>
-                <option value="2">2</option>
-
-                </select> */}
-
                 <button className="video-mute-toggle" onClick={toggleMute}>Mute</button>
-
               </div>
-
             </div>
-
           </div>
-
         </div>
-
         <div className='hero-drop-info-container'>
-
           <div className='hero-drop-info-container2'>
-      
             <div className='hero-drop-into-title'>DROP #1</div>
             <h1 className='hero-drop-title'>WHATEVER WEATHER</h1>
             <p id='hero-info'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
             <button id='hero-button'>ADD TO CART <sup className='sup-dollar'>$</sup>12.00</button>
-
             <div className="remaining__remainingProgressBar">                
               <h6 id="remaining">21 <span className='remaining-span'>of 100</span> Remaining</h6>
               <div id="remainingProgressBar"></div>
               <div id="remainingProgressBarStatus"></div>
             </div>
-
-
-
           </div>
-
         </div>
-
       </div>
       <div className='hero-bottom'>
-      <div className="countdown">
-              <h6 id="countdown__nextDrop">NEXT DROP</h6>
-              <h5 id="countdown__dropTitle">CLASSIC<br/> CARS</h5>
-              <h5 id="countdown__timeRemaining">JULY 1<sup className='sup'>ST</sup></h5>
-            </div>
+        <div className="countdown">
+          <h6 id="countdown__nextDrop">NEXT DROP</h6>
+          <h5 id="countdown__dropTitle">CLASSIC<br/> CARS</h5>
+          <h5 id="countdown__timeRemaining">JULY 1<sup className='sup'>ST</sup></h5>
+        </div>
       </div>
-
     </div>
 
   )
