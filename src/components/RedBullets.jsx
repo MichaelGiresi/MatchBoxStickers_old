e58056804972d6e73963344fa7b96cdceef30d4d
MichaelGiresi <mikegiresidev@gmail.com>
2022-09-02 19:20:47 -0400

    redbullets cleaned

--- src/components/RedBullets.jsx
+++ src/components/RedBullets.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import redBullets from '../styles/redBullets/redBullets.css'
+
 const RedBullets = () => {
-  return <div className='redBullets-container'>
-    <div className='redBullets-container1'>
-      <div className='redBullets-redCircle'></div>
+  return (
+    <div className='redBullets-container'>
+      <div className='redBullets-container1'>
+        <div className='redBullets-redCircle'></div>
         <div>&nbsp;New York designed & developed</div>
-    </div>
-    <div className='redBullets-container2'>
-      <div className='redBullets-redCircle'></div>
+      </div>
+      <div className='redBullets-container2'>
+        <div className='redBullets-redCircle'></div>
         <div>&nbsp;Each limited edition pack is signed & numbered</div>
-    </div>
-    <div className='redBullets-container3'>
-      <div className='redBullets-redCircle'></div>
+      </div>
+      <div className='redBullets-container3'>
+        <div className='redBullets-redCircle'></div>
         <div>&nbsp;Limit 2 per customer</div>
+      </div> 
     </div>
-        
-        </div>;
+  )
 };
 
 export default RedBullets;
